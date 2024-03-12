8487ae8c3c895bb8d0770f1ba4b06c5db57d47a2
Akshay Balamurugan <akbalamurugan@C02F71ENMD6M.local>
2024-03-12 18:07:23 +0530

    Initial Commit of MovieRails

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,36 @@
 class MoviesController < ApplicationController
     def index
+        @movies = Movie.all
+    end
+
+    def add
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(akshay_params)
+        if @movie.save
+            redirect_to "/"
+        else 
+            redirect_to "/about"
+        end
+    end
+
+    def about
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        Rails.logger.info akshay_params
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to "/"
+    end
+
+    def akshay_params
+        params.require(:movie).permit(:name,:director,:cast,:language,:cover)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  delete "/delete/:id" => "movies#destroy"
+  put "/update/:id" => "movies#update"
+
+  get "/about" => "movies#about"
+
+  get "/add" => "movies#add"
+  post "/add" => "movies#create"
+
   root "movies#index"
+
+  
 
   # Defines the root path route ("/")
   # root "posts#index"
